8f3b701b904cf6a44969067df4d236913ce1634a
NikitaKuzhel <nikita.kuzhel@gmail.com>
2021-11-12 14:29:10 +0200

    - added first homework

---
+++ Homework_01/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed = 2f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            targetMotion(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            targetMotion(Vector3.back);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            targetMotion(Vector3.left);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            targetMotion(Vector3.right);
+        }
+    }
+
+    void targetMotion (Vector3 direction)
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
