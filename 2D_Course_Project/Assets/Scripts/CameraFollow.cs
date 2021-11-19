99f23074b452f8731a9799a42dd6bc7f2b0e0b64
NikitaKuzhel <nikita.kuzhel@gmail.com>
2021-11-19 15:50:31 +0200

    - added character movement

---
+++ 2D_Course_Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float smoothSpeed = 0.125f;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+}
