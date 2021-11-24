156c4262948a9316ce93fcd64796c07e01b86ac3
NikitaKuzhel <nikita.kuzhel@gmail.com>
2021-11-24 16:56:01 +0200

    - added main menu and options UI

---
+++ 2D_Course_Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void MainGame()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
---
+++ 2D_Course_Project/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public void SetMasterVolume (float volume)
+    {
+        Debug.Log($"Master volume level is {volume}");
+    }
+
+    public void SetMusicVolume (float volume)
+    {
+        Debug.Log($"Music volume level is {volume}");
+    }
+
+    public void OpenCredits ()
+    {
+        Debug.Log("Credits button was presed");
+    }
+
+    public void SetQualityLevel(int qualityIndex)
+    {
+        Debug.Log($"Quality level index is: {qualityIndex}");
+    }
+
+    public void SetScreenResolution(int resolutionIndex)
+    {
+        Debug.Log($"Resolution index is: {resolutionIndex}");
+    }
+
+    public void FullsreenMode(bool isActive)
+    {
+        Debug.Log(isActive);
+    }
+}
