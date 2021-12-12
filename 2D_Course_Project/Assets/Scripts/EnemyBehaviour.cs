86923d25600635ef5c9ed3f2243caf533900d8aa
NikitaKuzhel <nikita.kuzhel@gmail.com>
2021-12-12 12:45:04 +0200

    - improved enemy AI

--- 2D_Course_Project/Assets/Scripts/EnemyBehaviour.cs
+++ 2D_Course_Project/Assets/Scripts/EnemyBehaviour.cs
@@ -5,23 +5,21 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     #region Public Variables
-    public Transform raycast;
-    public LayerMask raycastMask;
-    public float raycastLength;
     public float attackDistance; // минимальная дистанция для атаки
     public float moveSpeed;
     public float timer; // таймер для кулдауна между атаками
     public Transform leftLimit;
     public Transform rightLimit;
+    [HideInInspector] public Transform target;
+    [HideInInspector] public bool inRange; // проверяет, находится ли игрок в зоне
+    public GameObject hotZone;
+    public GameObject triggerArea;
     #endregion
 
     #region Private Variables
-    private RaycastHit2D hit;
-    private Transform target;
     private Animator anim;
     private float distance; // хранит дистанцию между врагом и игроком
     private bool attackMode;
-    private bool inRange; // проверяет, находится ли игрок в зоне
     private bool cooling; // проверяет, находится ли враг в куллдауне после атаки
     private float intTimer;
     #endregion
@@ -47,33 +45,7 @@ public class EnemyBehaviour : MonoBehaviour
 
         if (inRange)
         {
-            hit = Physics2D.Raycast(raycast.position, transform.right, raycastLength, raycastMask);
-            RaycastDebugger();
-        }
-
-        // Когда игрок был обнаружен
-        if (hit.collider != null)
-        {
             EnemyLogic();
-        }
-        else if (hit.collider == null)
-        {
-            inRange = false;
-        }
-
-        if (inRange == false)
-        {
-            StopAttack();
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D trig)
-    {
-        if (trig.gameObject.tag == "Player")
-        {
-            target = trig.transform;
-            inRange = true;
-            Flip();
         }
     }
 
@@ -136,18 +108,6 @@ public class EnemyBehaviour : MonoBehaviour
         anim.SetBool("Attack", false);
     }
 
-    void RaycastDebugger()
-    {
-        if (distance > attackDistance)
-        {
-            Debug.DrawRay(raycast.position, transform.right * raycastLength, Color.red);
-        }
-        else if (attackDistance > distance)
-        {
-            Debug.DrawRay(raycast.position, transform.right * raycastLength, Color.green);
-        }
-    }
-
     public void TrigerCooling()
     {
         cooling = true;
@@ -158,7 +118,7 @@ public class EnemyBehaviour : MonoBehaviour
         return transform.position.x > leftLimit.position.x && transform.position.x < rightLimit.position.x;
     }
 
-    private void SelectTarget()
+    public void SelectTarget()
     {
         float distanceToLeft = Vector2.Distance(transform.position, leftLimit.position);
         float distanceToRight = Vector2.Distance(transform.position, rightLimit.position);
@@ -175,7 +135,7 @@ public class EnemyBehaviour : MonoBehaviour
         Flip();
     }
 
-    private void Flip()
+    public void Flip()
     {
         Vector3 rotation = transform.eulerAngles;
 
