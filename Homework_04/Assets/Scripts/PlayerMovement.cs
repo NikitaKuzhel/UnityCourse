f4c115ceb585c3fb1c396b9b3b4d396bb50f23cb
NikitaKuzhel <nikita.kuzhel@gmail.com>
2021-12-27 16:09:03 +0200

    - added homework

---
+++ Homework_04/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 2f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            targetMotion(Vector3.back);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            targetMotion(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            targetMotion(Vector3.right);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            targetMotion(Vector3.left);
+        }
+    }
+
+    void targetMotion (Vector3 direction)
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
