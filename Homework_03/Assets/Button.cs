1d3248e44297eb5d326efc418cf1a46cc0a638fa
NikitaKuzhel <nikita.kuzhel@gmail.com>
2021-12-27 11:30:46 +0200

    - fixed something

--- Homework_03/Assets/Button.cs
+++ Homework_03/Assets/Button.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-
     [SerializeField] private Explosion explosion;
+
     // Start is called before the first frame update
     void Start()
     {
