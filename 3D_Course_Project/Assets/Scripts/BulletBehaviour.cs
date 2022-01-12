c15ad7806614be6c080a56b0bd1c38600c50d517
Nikita Kuzhel <nikita.kuzhel@gmail.com>
2022-01-12 14:53:23 +0200

    - update player controller

---
+++ 3D_Course_Project/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    [SerializeField] private float _autoDestroyTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+
+    private void StartAutoDestroy()
+    {
+        StartCoroutine(AutoDestroy());
+    }
+
+    private IEnumerator AutoDestroy()
+    {
+        yield return new WaitForSeconds(_autoDestroyTime);
+        DestroyBullet();
+    }
+}
--- 3D_Course_Project/Assets/Scripts/MovableSystem.cs
+++ 3D_Course_Project/Assets/Scripts/MovableSystem.cs
@@ -6,11 +6,15 @@ public class MovableSystem : MonoBehaviour
 {
     [SerializeField] private JoystickDetector _movable;
     [SerializeField] private float _speed = 12f;
+    [SerializeField] private float _rotationSpeed = 10f;
+    [SerializeField] private Animator _animator;
+
+    private Rigidbody _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,9 +25,21 @@ public class MovableSystem : MonoBehaviour
 
     private void Move()
     {
+        Vector3 playerVector = _movable.MotionVector;
+        playerVector = new Vector3(playerVector.x, 0, playerVector.y);
         if (_movable.IsMoved)
         {
-            transform.position += _movable.MotionVector * Time.deltaTime * _speed;
+            transform.position += playerVector * Time.deltaTime * _speed;
+            _animator.SetBool("IsRunning", true);
+            if (playerVector != Vector3.zero)
+            {
+                Quaternion toRotation = Quaternion.LookRotation(playerVector, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _rotationSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            _animator.SetBool("IsRunning", false);
         }
     }
 }
