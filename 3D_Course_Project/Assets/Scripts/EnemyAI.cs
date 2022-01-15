c138ad6a621ef53faf7cd20e5836e7a61fa549eb
Nikita Kuzhel <nikita.kuzhel@gmail.com>
2022-01-15 13:09:48 +0200

    - added enemy AI

---
+++ 3D_Course_Project/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private Transform _player;
+    [SerializeField] Animator _animator;
+    private ShootingBehaviour _shoot;
+
+    [SerializeField] private LayerMask _whatIsGround;
+    [SerializeField] private LayerMask _whatIsPlayer;
+
+    // Патрулирование
+    [SerializeField] private Vector3 _walkPoint;
+    [SerializeField] private float _walkPointRange;
+    private bool _walkPointSet;
+
+    // Атака
+    [SerializeField] private float _timeBetweenAttacks;
+    private bool _alreadyAttacked;
+
+    // Состояния
+    [SerializeField] private float _sightRange;
+    [SerializeField] private float _attackRange;
+    [SerializeField] private bool _playerInSightRange;
+    [SerializeField] private bool _playerInAttackRange;
+
+    private const float _maxDistance = 2f;
+
+
+    private void Awake()
+    {
+        _player = GameObject.Find("Player").transform;
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        // Проверка на область видимости и область атаки
+        _playerInSightRange = Physics.CheckSphere(transform.position, _sightRange, _whatIsPlayer);
+        _playerInAttackRange = Physics.CheckSphere(transform.position, _attackRange, _whatIsPlayer);
+
+        if (!_playerInSightRange && !_playerInAttackRange) Patroling();
+        if (_playerInSightRange && !_playerInAttackRange) ChasePlayer();
+        if (_playerInSightRange && _playerInAttackRange) AttackPLayer();
+    }
+
+    private void Patroling()
+    {
+        if (!_walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (_walkPointSet)
+        {
+            _agent.SetDestination(_walkPoint);
+            _animator.SetBool("IsRunning", true);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - _walkPoint;
+
+        // Если точка назначения достигнута
+        if (distanceToWalkPoint.magnitude < 1)
+        {
+            _walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        // Вычисляет ранодомную точку в радиусе
+        float randomZ = Random.Range(-_walkPointRange, _walkPointRange);
+        float randomX = Random.Range(-_walkPointRange, _walkPointRange);
+
+        _walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(_walkPoint, -transform.up, _maxDistance, _whatIsGround))
+        {
+            _walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        _agent.SetDestination(_player.position);
+    }
+
+    private void AttackPLayer()
+    {
+        // Удостовериться двигается ли враг
+        _agent.SetDestination(transform.position);
+
+        transform.LookAt(_player);
+
+        _animator.SetBool("IsRunning", false);
+
+        if (!_alreadyAttacked)
+        {
+            _shoot.Shoot();
+            Debug.Log("SHOOT");
+
+            _alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), _timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        _alreadyAttacked = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _sightRange);
+    }
+}
---
+++ 3D_Course_Project/Assets/Scripts/EnemyNavMeshMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavMeshMovement : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private Camera _cam;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
