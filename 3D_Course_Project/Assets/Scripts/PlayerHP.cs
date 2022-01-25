75b32fa6b35fc46bd486ac4ba58ea868df78ebcc
Nikita Kuzhel <nikita.kuzhel@gmail.com>
2022-01-25 17:50:37 +0200

    - added healthbar

--- 3D_Course_Project/Assets/Scripts/PlayerHP.cs
+++ 3D_Course_Project/Assets/Scripts/PlayerHP.cs
@@ -7,22 +7,50 @@ public class PlayerHP : MonoBehaviour
 {
     [SerializeField] private int _maxHealth = 100;
     [SerializeField] private int _currentHealth;
+    [SerializeField] private float _secondsBeforeRegen = 2f;
     [SerializeField] private Transform _respawnPoint;
+    private Coroutine _regen;
+
+    [SerializeField] private HealthBar _healthBar;
 
     void Start()
     {
         _currentHealth = _maxHealth;
+        _healthBar.SetMaxHealth(_maxHealth);
     }
 
     public void TakeDamage(int damage)
     {
         _currentHealth -= damage;
-        // Debug.Log("Taked damage");
+        _healthBar.SetHealth(_currentHealth);
 
         if (_currentHealth <= 0)
         {
             Debug.Log("Player Died!");
             transform.position = _respawnPoint.position;
+            _currentHealth = _maxHealth;
+            _healthBar.SetHealth(_currentHealth);
         }
+
+        if (_regen != null)
+        {
+            StopCoroutine(_regen);
+        }
+
+        _regen = StartCoroutine(RegenHealth());
+    }
+
+    private IEnumerator RegenHealth()
+    {
+        yield return new WaitForSeconds(_secondsBeforeRegen);
+
+        while (_currentHealth < _maxHealth)
+        {
+            _currentHealth += _maxHealth / 100;
+            _healthBar.SetHealth(_currentHealth);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        _regen = null;
     }
 }
---
+++ 3D_Course_Project/Assets/Scripts/UI/Buildboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buildboard : MonoBehaviour
+{
+    [SerializeField] private Transform _cam;
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + _cam.forward);
+    }
+}
