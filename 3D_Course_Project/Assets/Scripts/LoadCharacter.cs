13d2370827cc8cb6f4e1ed43fb3efc92f458d0a1
Nikita Kuzhel <nikita.kuzhel@gmail.com>
2022-01-27 15:49:34 +0200

    - added character selection UI

---
+++ 3D_Course_Project/Assets/Scripts/LoadCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadCharacter : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _characterPrefab;
+    [SerializeField] private Transform _spawnPoint;
+
+    void Start()
+    {
+        int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
+        GameObject prefab = _characterPrefab[selectedCharacter];
+        Instantiate(prefab, _spawnPoint.position, Quaternion.identity);
+    }
+}
--- 3D_Course_Project/Assets/Scripts/UI/MainMenu.cs
+++ 3D_Course_Project/Assets/Scripts/UI/MainMenu.cs
@@ -5,9 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject[] _character;
+    [SerializeField] private int _selectedCharacter = 0;
+
     public void PlayGame()
     {
+        BlueTeamScore.scoreValue = 0;
+        RedTeamScore.scoreValue = 0;
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void NextCharacter()
+    {
+        _character[_selectedCharacter].SetActive(false);
+        _selectedCharacter = (_selectedCharacter + 1) % _character.Length;
+        _character[_selectedCharacter].SetActive(true);
+    }
+
+    public void PreviousCharacter()
+    {
+        _character[_selectedCharacter].SetActive(false);
+        _selectedCharacter--;
+        if (_selectedCharacter < 0)
+        {
+            _selectedCharacter += _character.Length;
+        }
+        _character[_selectedCharacter].SetActive(true);
     }
 
     public void SetVolume(bool volume)
